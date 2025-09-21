8a5d5681b85bd15d73a12d61fd4c0a6d32329d1f
Routhu Balaji <routhubalaji111@gmail.com>
2025-09-21 12:14:33 +0530

    Learning Datatypes and Arrays in shell script

--- 09-datatypes.sh
+++ 09-datatypes.sh
@@ -1,6 +1,18 @@
 
 #!bin/bash
 
+
+# everything in shell is considerd as string
+
+NUMBER1=100
+NUMBER2=200
+NAME=DevOps
+
+SUM=$(($NUMBER1+$NUMBER2+$NAME))
+
+echo "SUM is: ${SUM}"
+
+
 Leaders=("Modi", "Putin","Trump")
 
 echo "Popular Leaders are $Leaders[@]"
