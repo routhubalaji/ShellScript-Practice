7c1ad868214bdc9e446e44fa6b81c180a0082af5
Routhu Balaji <routhubalaji111@gmail.com>
2025-09-22 19:16:42 +0530

    conditional statements

---
+++ 10-conditional_statements.sh
@@ -0,0 +1,25 @@
+
+#!bin/bash
+
+NUMBER = $1
+
+if [ $NUMBER -gt 10 ]
+then
+echo "Given Number $Number is greater than 10"
+else
+echo "Given Number $Number is less than than 10"
+fi
+
+if [ $NUMBER -lt 10]
+then
+echo "Given Number $Number is less than 10"
+else
+echo "Given Number $Number is greater than than 10"
+fi
+
+if [ $NUMBER -eq 10]
+then
+echo "Given Number $Number is equal to 10"
+else
+echo "Given Number $Number is not equal to 10"
+fi
