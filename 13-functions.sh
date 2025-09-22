d86f4652f25bf20045feed9c5a7c591c7e933e0c
Routhu Balaji <routhubalaji111@gmail.com>
2025-09-22 21:51:02 +0530

    session-3

---
+++ 13-functions.sh
@@ -0,0 +1,32 @@
+
+#!bin/bash
+  
+ if [ $USERID -ne 0 ];
+then
+    echo "ERROR:: Please run the script with root privilege"
+    exit 1
+fi
+
+
+
+VALIDATE() {
+    if [ $1 -ne 0 ];
+    then
+        echo "ERROR:: $2 Installation failed"
+        exit 1
+    else
+        echo "SUCCESS:: Mysql Installation successful"    
+        exit 0
+    fi 
+}
+
+
+
+dnf install mysql -y
+VALIDATE $? "Mysql"
+
+dnf install nginx -y
+VALIDATE $? "nginx"
+
+dnf install mongodb-mongosh -y
+VALIDATE $? "mongosh"
