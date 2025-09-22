89656935a5328f4d195c8fa5dea6097f4dd7fcd6
Routhu Balaji <routhubalaji111@gmail.com>
2025-09-22 22:48:26 +0530

    session-3

--- 15-colors.sh
+++ 15-colors.sh
@@ -20,10 +20,10 @@ fi
 VALIDATE() {
     if [ $1 -ne 0 ];
     then
-        echo "Installing $2... $R Failed $N"
+        echo -e "Installing $2... $R Failed $N"
         exit 1
     else
-        echo "Installing $2... $G Success $N"    
+        echo -e "Installing $2... $G Success $N"    
         # exit 0  If we uncomment this line then remaining 2 pacakges will not be installed.
     fi 
 }
