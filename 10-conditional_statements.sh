121cedafd15ee98fbe8405edf4daf37547357095
Routhu Balaji <routhubalaji111@gmail.com>
2025-09-22 19:30:22 +0530

    session-3

--- 10-conditional_statements.sh
+++ 10-conditional_statements.sh
@@ -10,16 +10,16 @@ NUMBER=$1
 # echo "Given Number $Number is less than than 10"
 # fi
 
-if [ $NUMBER -lt 10 ];
-then
-echo "Given Number $Number is less than 10"
-else
-echo "Given Number $Number is greater than than 10"
-fi
-
-# if [ $NUMBER -eq 10 ];
+# if [ $NUMBER -lt 10 ];
 # then
-# echo "Given Number $Number is equal to 10"
+# echo "Given Number $Number is less than 10"
 # else
-# echo "Given Number $Number is not equal to 10"
+# echo "Given Number $Number is greater than than 10"
 # fi
+
+if [ $NUMBER -eq 10 ];
+then
+echo "Given Number $Number is equal to 10"
+else
+echo "Given Number $Number is not equal to 10"
+fi
