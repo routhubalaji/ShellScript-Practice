8fddd1eca52cadac002cc206597ba4b51869a9b1
Routhu Balaji <routhubalaji111@gmail.com>
2025-09-22 20:17:53 +0530

    session-3

--- 12-installlation.sh
+++ 12-installlation.sh
@@ -3,11 +3,12 @@
 
 USERID=$(id -u)
 
-echo "user id $USERID"
+# echo "user id $USERID"
 
 if[ $USERID -ne 0 ];
 then
     echo "ERROR:: Please run with root privilege"
+    exit(1)
 fi
 
 dnf install mysql -y
@@ -16,6 +17,8 @@ dnf install mysql -y
 if[ $? -ne 0 ];
 then
     echo "ERROR:: Mysql Installation failed"
+    exit(1)
 else
     echo "SUCCESS:: Mysql Installation successful"    
+    exit(0)
 fi
