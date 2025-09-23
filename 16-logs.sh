c767d668e9139d3af225b761caeffd5070e7623d
Routhu Balaji <routhubalaji111@gmail.com>
2025-09-23 23:37:51 +0530

    session-4

--- 16-logs.sh
+++ 16-logs.sh
@@ -16,11 +16,6 @@ SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
 
 
-
-# LOGS_FOLDER="/var/log/shell-script"
-# SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
-# LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
-
 mkdir -p $LOGS_FOLDER
 
 echo "Script started executed at: $(date)" 
