836d716314d73ad2a784a9cde44e48d3710e87cb
Routhu Balaji <routhubalaji111@gmail.com>
2025-09-23 23:30:37 +0530

    session-4

--- 16-logs.sh
+++ 16-logs.sh
@@ -15,7 +15,7 @@ LOGS_FOLDER="/var/log/shell-script"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
 echo "$SCRIPT_NAME"
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
-echo "$LOG_FILE"
+echo "log file name $LOG_FILE"
 
 mkdir -p $LOGS_FOLDER
 
