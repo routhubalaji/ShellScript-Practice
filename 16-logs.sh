45844024b327533a895efff9ddd3bb2355098d82
Routhu Balaji <routhubalaji111@gmail.com>
2025-09-23 22:48:22 +0530

    session-4

--- 16-logs.sh
+++ 16-logs.sh
@@ -1,71 +1,123 @@
-#!bin/bash
+# #!bin/bash
 
 
+
+# USERID=$(id -u)
+
+# R="\e[31m"
+# G="\e[32m"
+# Y="\e[33m"
+# N="\e[0m"
+
+
+
+# LOGS_FOLDER="/var/log/shell-script"
+# SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+# LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
+
+# mkdir -p $LOGS_FOLDER
+
+# echo "Script started executed at: $(date)" 
+# if [ $USERID -ne 0 ];
+# then
+#     echo -e "$R ERROR:: Please run the script with root privilege... $N"
+#     exit 1
+# fi
+
+
+
+# VALIDATE() {
+#     if [ $1 -ne 0 ];
+#     then
+#         echo -e "Installing $2... $R Failed $N" 
+#         exit 1
+#     else
+#         echo -e "Installing $2... $G Success $N"    
+#         # exit 0  If we uncomment this line then remaining 2 pacakges will not be installed.
+#     fi 
+# }
+
+
+# dnf list installed mysql &>> LOGS_FILE
+# if [ $? -ne 0 ];
+# then
+#     dnf install mysql -y &>> LOGS_FILE
+#     VALIDATE $? "Mysql"
+# else
+#     echo -e "MySQL already exist ... $Y SKIPPING $N"    
+# fi    
+
+# dnf list installed nginx &>> LOGS_FILE
+# if [ $? -ne 0 ];
+# then
+#     dnf install nginx -y &>> LOGS_FILE
+#     VALIDATE $? "nginx"
+# else
+#     echo -e "Nginx already exist ... $Y SKIPPING $N"        
+# fi  
+
+
+# dnf list installed python3 &>> LOGS_FILE
+# if [ $? -ne 0 ];
+# then
+#     dnf install python3 -y
+#     VALIDATE $? "Python3" &>> LOGS_FILE
+# else
+#     echo -e "python3 already exist ... $Y SKIPPING $N"    
+# fi
+
+
+
+
+#!/bin/bash
 
 USERID=$(id -u)
-
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
-
-# LOGS_FOLDER="/var/log/shell-script"
-# SCRIPT_NAME=$( echo $0 | cut -d "." -f1)
-# LOGS_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
-
 
 LOGS_FOLDER="/var/log/shell-script"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
 
 mkdir -p $LOGS_FOLDER
-
 echo "Script started executed at: $(date)" 
-if [ $USERID -ne 0 ];
-then
-    echo -e "$R ERROR:: Please run the script with root privilege... $N"
-    exit 1
+
+if [ $USERID -ne 0 ]; then
+    echo "ERROR:: Please run this script with root privelege"
+    exit 1 # failure is other than 0
 fi
 
-
-
-VALIDATE() {
-    if [ $1 -ne 0 ];
-    then
-        echo -e "Installing $2... $R Failed $N" 
+VALIDATE(){ # functions receive inputs through args just like shell script args
+    if [ $1 -ne 0 ]; then
+        echo -e "Installing $2 ... $R FAILURE $N" 
         exit 1
     else
-        echo -e "Installing $2... $G Success $N"    
-        # exit 0  If we uncomment this line then remaining 2 pacakges will not be installed.
-    fi 
+        echo -e "Installing $2 ... $G SUCCESS $N" 
 }
 
-
-dnf list installed mysql &>> LOGS_FILE
-if [ $? -ne 0 ];
-then
-    dnf install mysql -y &>> LOGS_FILE
-    VALIDATE $? "Mysql"
+dnf list installed mysql &>>$LOG_FILE
+# Install if it is not found
+if [ $? -ne 0 ]; then
+    dnf install mysql -y &>>$LOG_FILE
+    VALIDATE $? "MySQL"
 else
-    echo -e "MySQL already exist ... $Y SKIPPING $N"    
-fi    
-
-dnf list installed nginx &>> LOGS_FILE
-if [ $? -ne 0 ];
-then
-    dnf install nginx -y &>> LOGS_FILE
-    VALIDATE $? "nginx"
-else
-    echo -e "Nginx already exist ... $Y SKIPPING $N"        
-fi  
-
-
-dnf list installed python3 &>> LOGS_FILE
-if [ $? -ne 0 ];
-then
-    dnf install python3 -y
-    VALIDATE $? "Python3" &>> LOGS_FILE
-else
-    echo -e "python3 already exist ... $Y SKIPPING $N"    
+    echo -e "MySQL already exist ... $Y SKIPPING $N"
 fi
 
+dnf list installed nginx &>>$LOG_FILE
+if [ $? -ne 0 ]; then
+    dnf install nginx -y &>>$LOG_FILE
+    VALIDATE $? "Nginx"
+else
+    echo -e "Nginx already exist ... $Y SKIPPING $N" 
+fi
+
+dnf list installed python3 &>>$LOG_FILE
+if [ $? -ne 0 ]; then
+    dnf install python3 -y &>>$LOG_FILE
+    VALIDATE $? "python3"
+else
+    echo -e "Python3 already exist ... $Y SKIPPING $N" 
+fi
