1f4868ecbcfaf68299ecc8e9776c7e4a462580b6
Routhu Balaji <routhubalaji111@gmail.com>
2025-09-23 22:36:36 +0530

    session-4

---
+++ 16-logs.sh
@@ -0,0 +1,66 @@
+#!bin/bash
+
+
+
+USERID=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOGS_FOLDER="/var/log/shell-script"
+SCRIPT_NAME=$( echo $1 | cut -d "." -f1)
+LOGS_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+
+mkdir -p $LOGS_FOLDER
+
+echo "Script started executed at: $(date)" 
+if [ $USERID -ne 0 ];
+then
+    echo -e "$R ERROR:: Please run the script with root privilege... $N"
+    exit 1
+fi
+
+
+
+VALIDATE() {
+    if [ $1 -ne 0 ];
+    then
+        echo -e "Installing $2... $R Failed $N" 
+        exit 1
+    else
+        echo -e "Installing $2... $G Success $N"    
+        # exit 0  If we uncomment this line then remaining 2 pacakges will not be installed.
+    fi 
+}
+
+
+dnf list installed mysql &>> LOGS_FILE
+if [ $? -ne 0 ];
+then
+    dnf install mysql -y &>> LOGS_FILE
+    VALIDATE $? "Mysql"
+else
+    echo -e "MySQL already exist ... $Y SKIPPING $N"    
+fi    
+
+dnf list installed nginx &>> LOGS_FILE
+if [ $? -ne 0 ];
+then
+    dnf install nginx -y &>> LOGS_FILE
+    VALIDATE $? "nginx"
+else
+    echo -e "Nginx already exist ... $Y SKIPPING $N"        
+fi  
+
+
+dnf list installed python3 &>> LOGS_FILE
+if [ $? -ne 0 ];
+then
+    dnf install python3 -y
+    VALIDATE $? "Python3" &>> LOGS_FILE
+else
+    echo -e "python3 already exist ... $Y SKIPPING $N"    
+fi
+
