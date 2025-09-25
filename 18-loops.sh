3046744eceb3739a6fe86b46d8c74d0c40c57604
Routhu Balaji <routhubalaji111@gmail.com>
2025-09-25 23:28:20 +0530

    session-4

--- 18-loops.sh
+++ 18-loops.sh
@@ -70,7 +70,15 @@ VALIDATE() {
 
 for package in $@
 do
-    echo $package
+     dnf list installed $package &>>$LOG_FILE
+
+    # if exit status is 0, already installed. -ne 0 need to install it
+    if [ $? -ne 0 ]; then
+        dnf install $package -y &>>$LOG_FILE
+        VALIDATE $? "$package"
+    else
+        echo -e "$package already installed ... $Y SKIPPING $N"
+    fi
 done
 
 
